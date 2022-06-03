6feb3b08a01bd6f4d040cea005b03d7169ca90ab
Ty <ty.rickers@gmail.com>
2022-06-03 13:59:02 -0600

    created chat component

--- client/write-right-draw/src/components/Chat.js
+++ client/write-right-draw/src/components/Chat.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import io from 'socket.io-client'
+import { useEffect } from 'react'
 const socket = io.connect("http://localhost:3001")
 
 function Chat() {
@@ -7,6 +8,13 @@ function Chat() {
   const sendMessage = () => {
     socket.emit("send_message", {message: "Hello"})
   }
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      alert(data.message)
+    })
+  }, [socket])
+
 
   return (
     <div>
