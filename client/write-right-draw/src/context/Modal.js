f386cf9a944c18b15303112a12707eaa62356813
Ty Rickers <ty.rickers@gmail.com>
2022-08-10 10:48:53 -0600

    created modal component and modal.css

--- client/write-right-draw/src/context/Modal.js
+++ client/write-right-draw/src/context/Modal.js
@@ -1,4 +1,6 @@
 import React, { useContext, useRef, useState, useEffect } from "react";
+import { ReactDOM } from "react-dom";
+import "./Modal.css";
 
 //This can be the start of my a la Modal character who has an ice cream head and uses modal verbs
 
@@ -18,5 +20,19 @@ const ModalProvider = ({ children }) => {
     </>
   );
 };
+
+export function Modal(onClose, children) {
+  const modalNode = useContext(ModalContext);
+
+  if (modalNode === false) return null;
+
+  return ReactDOM.createPortal(
+    <div id="modal">
+      <div id="modal-background" onClick={onClose}></div>
+      <div id="modal-content">{children}</div>
+    </div>,
+    modalNode
+  );
+}
 
 export default ModalProvider;
